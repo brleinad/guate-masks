92114511eba9a44f6e480aa9734c04117877a699
Daniel Rodas Bautista <daniel@Paules-MacBook-Air.local>
2020-10-17 21:56:42 -0400

    fixes env and text styling

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   URL: 'https://www.guatemasques.ca',
-  API_URL: 'https://www.guatemasques.ca/.netlify/functions/',
+  API_URL: 'https://www.guatemasques.ca/.netlify/functions',
   STRIPE_KEY : 'pk_live_51HZn5VFQZ3uzVtBw9h7oddkOC9G1ZRnd0FVBtHj4crNmPVnyzE7SjFkKqoRVYyDOt8I9pPpUMcRxPgxjN2SgaXDV00Q0iESOF9',
   HUIPIL_PRICE_KEY : 'price_1HdMP8FQZ3uzVtBwodjx9EsT',
   CORTE_PRICE_KEY : 'price_1HdMPbFQZ3uzVtBw6DL4CEji',
