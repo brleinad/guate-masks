223c62b090a14155ddf88e03d050614a09bfca32
brleinad <daniel.rodas.bautista@gmail.com>
2020-10-26 21:50:39 -0400

    fixing routes

--- scully.guate-masks.config.ts
+++ scully.guate-masks.config.ts
@@ -2,17 +2,22 @@ import { ScullyConfig } from '@scullyio/scully';
 import {Masks} from './scully/plugins/masks-plugin';
 
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "guate-masks",
+  projectRoot: './src',
+  projectName: 'guate-masks',
   outDir: './dist/static',
   routes: {
+    'mask/:id': {
+      type: 'json',
+      id: {
+        url: '',
+        property: 'id'
+      }
+    }
+  }
+/*   routes: {
     '/mask/:id': {
       type: Masks,
-      // url: 'https://raw.githubusercontent.com/brleinad/guate-masks/main/db.json',
       url: 'https://www.guatemasques.ca/.netlify/functions/get-masks',
-/*       id: {
-        property: '',
-      }, */
-    },
-  }
+      },
+    }, */
 };
